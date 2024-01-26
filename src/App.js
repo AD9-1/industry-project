0f8cc7223d59e9791bb13717ecb1d73c11f44ecc
Aparna Dhara <aparnadharaa@gmail.com>
2024-01-26 10:20:38 -0800

    creating watsapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
+import MessageBody from "./Components/MessageBody/MessageBody";
 import MessageInput from "./Components/MessageInput/MessageInput";
 function App() {
   return (
     <div className="App">
       <Header />
-      <MessageInput />
+      <MessageBody />
     </div>
   );
 }
