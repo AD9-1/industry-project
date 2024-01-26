476218d9c93485d0faae01d2706a548e1f28c557
Aparna Dhara <aparnadharaa@gmail.com>
2024-01-25 17:26:38 -0800

    creating watsapp

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,29 @@
 import React from "react";
-
+import "./Header.scss";
+import cameraIcon from "../../assets/video-solid.svg";
+import callIcon from "../../assets/phone-solid.svg";
+import backarrow from "../../assets/arrow-left-solid.svg";
 const Header = () => {
   return (
     <div className="wa-header">
-      <div></div>
-      <div></div>
-      <div className="wa-header__info">
-        <div className="wa-header__info__name">Jayme</div>
-        <div className="wa-header__info__time">3:58pm</div>
+      <div className="wa-header__back">
+        <img src={backarrow}/>
       </div>
-      <div></div>
-      <div></div>
+      <div className="wa-header__image"></div>
+      <div className="wa-header__info">
+        <div className="wa-header__info__name">
+          <p>Jayme</p>
+        </div>
+        <div className="wa-header__info__time"><p>3:58pm</p></div>
+      </div>
+
+      <div className="wa-header__icons">
+        <img className="wa-header__icons__video" src={cameraIcon}></img>
+      </div>
+
+      <div className="wa-header__icons">
+        <img className="wa-header__icons__call"src={callIcon}></img>
+      </div>
       <div></div>
     </div>
   );
