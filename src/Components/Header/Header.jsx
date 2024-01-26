8fabae00000177a9677e80b2ccb620574d1e71c8
Aparna Dhara <aparnadharaa@gmail.com>
2024-01-25 16:07:19 -0800

    creating watsapp

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="wa-header">
+      <div></div>
+      <div></div>
+      <div className="wa-header__info">
+        <div className="wa-header__info__name">Jayme</div>
+        <div className="wa-header__info__time">3:58pm</div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
